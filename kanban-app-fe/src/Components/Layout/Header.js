3d8460130bebe1a26e75185ea04538053d201eb5
LAPTOP-Q2C2TQ93\Admin <hikari.mei.119@gmail.com>
2023-06-10 23:24:29 +0900

    dev_B testGit

--- kanban-app-fe/src/Components/Layout/Header.js
+++ kanban-app-fe/src/Components/Layout/Header.js
@@ -6,6 +6,7 @@ class Header extends Component {
     return (
       <div>
         <h1>dev_A TestGit</h1>
+        <h2>dev_B testGit</h2>
         <nav className="navbar navbar-expand-sm navbar-dark bg-primary mb-4">
           <div className="container">
             <a className="navbar-brand" href="Dashboard.html">
